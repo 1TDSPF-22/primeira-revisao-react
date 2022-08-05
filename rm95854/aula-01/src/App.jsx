e30a32cb70c2a1d3faa02ada4727eab409994221
eduardacavvalheiro <rm95854@fiap.com.br>
2022-08-05 12:23:49 -0300

    alterações realizadas

---
+++ rm95854/aula-01/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import Primeiro from "./components/Primeiro"
+import "./App.css"
+
+
+export default function App(){
+
+    const tag = <strong>Olá React!!</strong>
+
+    return (
+    <div>
+        <h1>{tag}</h1>
+        <h2>Novo elemento</h2>
+        <h3>Mais um título</h3>
+        <Primeiro></Primeiro>
+    </div>
+    )
+}
---
+++ rm95854/aula-01/src/components/Primeiro.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+export default function Primeiro(){
+    return (
+        <div>
+            <h2>Sou um primeiro componente filho!</h2>
+        </div>
+    )
+}
