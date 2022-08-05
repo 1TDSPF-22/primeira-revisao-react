7f40bafc3617e039f1cb8c468ebb9fbefc9677ec
NB2P <leo.p.longo@hotmail.com>
2022-08-05 12:23:10 -0300

    Criado meu primeiro app react!

---
+++ aula-01/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Primeiro from "./components/Primeiro";
+import "./App.css";
+
+export default function App() {
+	const tag = <strong>Olá, React!</strong>;
+
+	return (
+		<div>
+			<h1>{tag}</h1>
+			<h2>Novo Elemento</h2>
+			<h3>Outro Título</h3>
+			<Primeiro></Primeiro>
+		</div>
+	);
+}
