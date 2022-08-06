3d78848d87d7f1c5d68726700c0a82037e153f48
phalves23 <rm93693@fiap.com.br>
2022-08-06 13:29:19 -0300

    :fire: remove code

--- pf0670/aula-01/src/components/Primeiro.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Primeiro(){
-    return(
-        <div>
-            <h2>Sou um primeiro componente filho!</h2>
-        </div>
-    )
-}
--- pf0670/aula-01/src/index.js
+++
@@ -1,8 +0,0 @@
-//importando React para garantir a herança do JSX
-import React from 'react'
-//IMPORTANDO O VDOM
-import ReactDOM from 'react-dom'
-import App from './App'
-
-
-ReactDOM.render(<App/>, document.getElementById("root"))
