a0cf371604fb40f1cccd441066792b4db148ae5a
phalves23 <rm93693@fiap.com.br>
2022-08-06 13:27:17 -0300

    :sparkles: first react class

---
+++ rm93693/aula-01/src/index.js
@@ -0,0 +1,7 @@
+//importando React para garantir a herança do JSX
+import React from "react";
+//IMPORTANDO O VDOM
+import ReactDOM from "react-dom";
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
