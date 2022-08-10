843e5752bd35155f7c1fb32df8f81c1c1d2b6df4
ViníciusYoda <rm92969@fiap.com.br>
2022-08-10 14:30:15 -0300

    exercicioexercicios

--- rm92969/exercicio01/src/components/Carro/Carro.jsx
+++ rm92969/exercicio01/src/components/Carro/Carro.jsx
@@ -3,17 +3,20 @@ import React from 'react'
 import carro1 from '../../img/carro.jpg'
 import Parceiros from '../Parceiros/Parceiros'
 
-export default function Carro(props){
+export default function Carro(){
+
+    const novoModelo = () => 'Equinox'
 
     return(
         <>
         <img src={carro1}width="250px" alt="Carro" />
         <ul>
-            <li>{props.modelo["Fusca"]}</li>
-            <li></li>
             <li>Fusca</li>
-            <li>Fusca</li>
-            <li>Fusca</li>
+            <li>Onix</li>
+            <li>Cruze</li>
+            <li>Spin</li>
+            <li>Camaro</li>
+            <Parceiro carros={carros} novoModelo={novoModelo} />
         </ul>
         <Parceiros></Parceiros>
         </>
--- rm92969/exercicio01/src/components/Parceiros/Parceiros.jsx
+++ rm92969/exercicio01/src/components/Parceiros/Parceiros.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
-export default function Parceiros(){
+export default function Parceiros(props){
     return(
         <>
             <h2>Esses são nossos parceiros</h2>
-            <p><a href="#">Ford</a></p>
-            <p><a href="#">Ford</a></p>
-            <p><a href="#">Ford</a></p>
-            <p><a href="#">Ford</a></p>
+            <p><a href="#">{props.parceiros['Ford']}</a></p>
+            <p><a href="#">{props.parceiros['Ford']}</a></p>
+            <p><a href="#">{props.parceiros['Ford']}</a></p>
+            <p><a href="#">{props.parceiros['Ford']}</a></p>
+            <p><a href="#">{props.novoModelo()}</a></p>
         </>
     )
 }
